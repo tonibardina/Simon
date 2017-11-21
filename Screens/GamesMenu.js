02096b37ad69668daeb87774dcc07f90da9b9ab0
Toni Bardina <tonibardina@gmail.com>
2017-11-21 15:47:19 +0100

    cheking back-end

--- Screens/GamesMenu.js
+++ Screens/GamesMenu.js
@@ -25,8 +25,8 @@ class GamesMenu extends Component {
           <Image style={{width: 200, height: 150, resizeMode: 'stretch', backgroundColor: 'transparent'}} source={require('../art/tittlex3.png')} />
         </View>
         <View style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center'}}>
-          <TouchableHighlight onPress={this.chooseGame1} style={{width: 100, alignItems: 'center', borderRadius: 20}}>
-            <Text style={{backgroundColor: '#040419', color: 'white', padding: 10, width: 150, textAlign: 'center', borderRadius: 10, top: 150}}>
+          <TouchableHighlight onPress={this.chooseGame1} style={{width: 100, alignItems: 'center', borderRadius: 20, backgroundColor: 'red', top: 150}}>
+            <Text style={{backgroundColor: '#040419', color: 'white', padding: 10, width: 150, textAlign: 'center', borderRadius: 10}}>
               Play
             </Text>
           </TouchableHighlight>
