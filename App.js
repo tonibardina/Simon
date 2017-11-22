ae903e62eeb076b2c002ce636a70b468f5f5da1f
Toni Bardina <tonibardina@gmail.com>
2017-11-22 16:35:46 +0100

    bugs correction

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
 import React, { Component } from 'react'
 import {
-  StyleSheet,
+  AsyncStorage,
   View
 } from 'react-native'
 import GamesMenu from './Screens/GamesMenu'
 import Background from './Components/GameComponents/Background'
 import Levels from './Screens/Levels'
 import Level1 from './Screens/Level1/Level1'
+import RegisterPage from './Components/GameComponents/RegisterPage'
 
 export default class App extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      screen: 'menu'
+      screen: 'menu',
+      user: {
+        username: null,
+        points: 'points',
+        levelsUnBlocked: 1
+      }
     }
     this.setScreen = this.setScreen.bind(this)
     this.screen = this.screen.bind(this)
+    this.setUser = this.setUser.bind(this)
+  }
+
+  componentDidMount () {
+    //AsyncStorage.removeItem('username')
+    if (this.state.screen === 'noregistered' || this.state.screen === 'menu') {
+      AsyncStorage.getItem('username').then((value) => {
+        if (value === null) {
+          this.setState({
+            screen: 'noregistered'
+          })
+        } else {
+          this.setState({
+            user: {
+              username: value,
+              points: 'points',
+              levelsUnBlocked: 1
+            },
+            screen: 'menu'
+          })
+        }
+      })
+    }
+  }
+
+  setUser (value) {
+    AsyncStorage.setItem('username', value)
+    this.setState({
+      user: {
+        username: value,
+        points: 'points',
+        levelsUnBlocked: 1
+      }
+    })
   }
 
   setScreen (value) {
@@ -25,9 +65,10 @@ export default class App extends Component {
   }
 
   screen () {
-    if (this.state.screen === 'menu') { return <GamesMenu setScreen={this.setScreen} /> }
+    if (this.state.screen === 'menu') { return <GamesMenu setScreen={this.setScreen} username={this.state.user.username} /> }
     if (this.state.screen === 'levels') { return <Levels setScreen={this.setScreen} /> }
     if (this.state.screen === 'level1') { return <Level1 setScreen={this.setScreen} /> }
+    if (this.state.screen === 'noregistered') { return <RegisterPage setUser={this.setUser} setScreen={this.setScreen} /> }
   }
 
   render () {
--- Screens/GamesMenu.js
+++ Screens/GamesMenu.js
@@ -24,6 +24,11 @@ class GamesMenu extends Component {
         <View style={{position: 'absolute', top: 40, left: 0, width: '100%', height: '100%', justifyContent: 'flex-start', alignItems: 'center'}}>
           <Image style={{width: 200, height: 150, resizeMode: 'stretch', backgroundColor: 'transparent'}} source={require('../art/tittlex3.png')} />
         </View>
+        <View>
+          <Text style={{backgroundColor: 'transparent', color: 'white', fontSize: 30}}>
+            Hello {this.props.username} !
+          </Text>
+        </View>
         <View style={{position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center'}}>
           <TouchableHighlight onPress={this.chooseGame1} style={{width: 100, alignItems: 'center', borderRadius: 20}}>
             <Text style={{backgroundColor: '#040419', color: 'white', padding: 10, width: 150, textAlign: 'center', borderRadius: 10, top: 150}}>
